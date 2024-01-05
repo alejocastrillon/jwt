b15d6073fd0a7944db07e0a05fbb88b988838954
alejocastrillon <alejandro.castrillon@utp.edu.co>
2024-01-05 15:16:08 -0500

    Refactor JwtUtil.class

--- src/main/java/com/pichincha/alejandrocastrillon/jwt/infrastructure/JwtUtil.java
+++ src/main/java/com/pichincha/alejandrocastrillon/jwt/infrastructure/JwtUtil.java
@@ -4,13 +4,19 @@ import java.util.Date;
 
 import io.jsonwebtoken.Jwts;
 
-public class JwtUtil {
+public final class JwtUtil {
+
+    private JwtUtil() {
+    }
     
-    private static final String SECRET = "584534345443";
+    private static final String SECRET = "secret";
 
     private static final long EXPIRATION_TIME = 864_000_000;
 
     public static String generateToken(String username) {
-        return Jwts.builder().subject(username).expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME)).compact();
+        return Jwts.builder()
+            .subject(username)
+            .expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+            .compact();
     }
 }
